a832ef5e68ce0389ecfcc49a1d3e6da05c5ef84c
Erick Perez <erickperez@ericks-mac-mini.local>
2019-02-04 14:57:34 -0800

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Erick Perez on 2/4/19.
+//  Copyright © 2019 Erick Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
